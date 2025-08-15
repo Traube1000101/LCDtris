6a112419883ded2cfaeb73c5f01e6b5d91cb9927
Traube <girlscoutcookies420@gmx.net>
2025-08-16 00:35:49 +0200

    chore: naming, coments & layout

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,9 @@
 using std::bitset;
 
 // Predefine functions
-bool get_cell(int, int);
-void set_cell(int, int, bool);
+bool get_block(int, int);
+void set_block(int, int, bool);
+void initLcd();
 void render();
 void randomizeBoard(float);
 inline char bitsToChar(bool, bool);
@@ -17,15 +18,18 @@ void lcdWriteBlock(int, int, char);
 #define HEIGHT 20
 LiquidCrystal_I2C lcd(0x27, HEIGHT, WIDTH / 2);
 
+#define BACKGROUND 32 // Space char
+#define FULL_BLOCK 255
+// Custom left & right block chars
 #define LEFT_BLOCK 0
 #define RIGHT_BLOCK 1
-#define FULL_BLOCK 255
 #define BLOCKS(x, y) x, x, x, x, y, y, y, y
 uint8_t blocks[][8] = {{BLOCKS(0x0, 0xff)}, {BLOCKS(0xff, 0x0)}};
 
+// Bitset to store every block
 bitset<WIDTH * HEIGHT> board;
-bool get_cell(int x, int y) { return board[y * WIDTH + x]; }
-void set_cell(int x, int y, bool value) { board[y * WIDTH + x] = value; }
+bool get_block(int x, int y) { return board[y * WIDTH + x]; }
+void set_block(int x, int y, bool value) { board[y * WIDTH + x] = value; }
 
 OneButton *button[] = {new OneButton(D0, true, true),
                        new OneButton(D1, true, true)};
@@ -33,17 +37,8 @@ OneButton *button[] = {new OneButton(D0, true, true),
 // Buffer holding last written chars
 char shadow[WIDTH / 2][HEIGHT];
 
-void lcdInit() {
-  lcd.init();
-  lcd.backlight();
-  // Initialize with spaces
-  for (int r = 0; r < WIDTH / 2; ++r)
-    for (int c = 0; c < HEIGHT; ++c)
-      shadow[r][c] = ' ';
-}
-
 void setup() {
-  lcdInit();
+  initLcd();
 
   // Create left & right block chars
   for (int i = 0; i < 2; i++) {
@@ -81,15 +76,24 @@ void loop1() {
   button[1]->tick();
 }
 
+void initLcd() {
+  lcd.init();
+  lcd.backlight();
+  // Initialize with spaces
+  for (int r = 0; r < WIDTH / 2; ++r)
+    for (int c = 0; c < HEIGHT; ++c)
+      shadow[r][c] = ' ';
+}
+
 void render() {
   for (int row = 0; row < 4; ++row) {
     int xLeft = row * 2;
     int xRight = xLeft + 1;
     for (int col = 0; col < 20; ++col) {
       int y = col;
-      bool leftBit = (xLeft < WIDTH && y < HEIGHT) ? get_cell(xLeft, y) : false;
+      bool leftBit = (xLeft < WIDTH && y < HEIGHT) ? get_block(xLeft, y) : false;
       bool rightBit =
-          (xRight < WIDTH && y < HEIGHT) ? get_cell(xRight, y) : false;
+          (xRight < WIDTH && y < HEIGHT) ? get_block(xRight, y) : false;
       char out = bitsToChar(leftBit, rightBit);
       lcdWriteBlock(y, xLeft / 2, out);
     }
@@ -106,16 +110,18 @@ void randomizeBoard(float fillProbability) {
   }
 }
 
+// Convert left & right bits to single char
 inline char bitsToChar(bool leftBit, bool rightBit) {
-  static constexpr char lut[4] = {' ', LEFT_BLOCK, RIGHT_BLOCK, FULL_BLOCK};
+  static constexpr char lut[4] = {32, LEFT_BLOCK, RIGHT_BLOCK, FULL_BLOCK};
   return lut[(leftBit << 1) | rightBit];
 }
 
-void lcdWriteBlock(int col, int row, char ch) {
+void lcdWriteBlock(int col, int row, char block) {
+  // Write to lcd if within lcd bounds & char has changes
   if (col < 0 || col >= HEIGHT || row < 0 || row >= WIDTH / 2 ||
-      shadow[row][col] == ch)
+      shadow[row][col] == block)
     return;
   lcd.setCursor(col, row);
-  lcd.write(ch);
-  shadow[row][col] = ch;
+  lcd.write(block);
+  shadow[row][col] = block;
 }
